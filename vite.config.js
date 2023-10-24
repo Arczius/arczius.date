096a54e485ef94245379da68317c38d5ce9be33f
Arczius <arczius.development@gmail.com>
2023-10-24 13:44:53 +0200

    added some plugin's for vite to automatically check for eslint

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import cleanPlugin from 'vite-plugin-clean';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), cleanPlugin(), eslint()]
 });
